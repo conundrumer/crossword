f59d4c9a6ae9a16a40a3e5778be0513d6da7f5a6
David Lu <davidelu93@gmail.com>
2016-11-28 12:32:47 -0500

    separately keep track of min_area + add CollectionWithLast.last

--- src/filter.rs
+++ src/filter.rs
@@ -1,6 +1,7 @@
 use std::i16::MAX;
 use std::collections::HashSet;
 use std::collections::BTreeSet;
+use std::cell::Cell;
 use std::cell::RefCell;
 
 use placement::Position;
@@ -11,9 +12,17 @@ use crossword::Crossword;
 pub struct Filter {
     pub seen: RefCell<HashSet<WordPlacements>>,
     pub min_areas: RefCell<BTreeSet<i16>>,
+    min_area: Cell<i16>,
     has_min_areas: bool
 }
-
+trait CollectionWithLast<T> {
+    fn last(&self) -> Option<T>;
+}
+impl<T: Copy> CollectionWithLast<T> for BTreeSet<T> {
+    fn last(&self) -> Option<T> {
+        self.iter().next_back().map(|&x| x)
+    }
+}
 impl Filter {
     pub fn new(num_areas: usize) -> Filter {
         let mut min_areas = BTreeSet::new();
@@ -21,6 +30,7 @@ impl Filter {
         Filter {
             seen: RefCell::new(HashSet::new()),
             min_areas: RefCell::new(min_areas),
+            min_area: Cell::new(MAX),
             has_min_areas: num_areas > 0
         }
     }
@@ -32,7 +42,7 @@ impl Filter {
             return true
         }
         let area = bb.combine_word_pos(word, next_pos).area();
-        area <= self.min_area()
+        area <= self.min_area.get()
     }
     pub fn by_seen(&self, crossword: &Crossword, num_remaining_words: usize) -> bool {
         {
@@ -43,19 +53,16 @@ impl Filter {
             seen.insert(crossword.positions.clone());
         }
         if self.has_min_areas && num_remaining_words == 1 {
-            let min_area = self.min_area();
+            let min_area = self.min_area.get();
             let mut min_areas = self.min_areas.borrow_mut();
 
             let area = crossword.bounding_box().area();
             if area < min_area && !min_areas.contains(&area) {
                 min_areas.remove(&min_area);
                 min_areas.insert(area);
+                self.min_area.set(min_areas.last().unwrap());
             }
         }
         true
-    }
-    fn min_area(&self) -> i16 {
-        let min_areas = self.min_areas.borrow();
-        *min_areas.iter().next_back().unwrap()
     }
 }
